fda8496ec8fb4169b687251858bce1bf00c7022e
Jasonm23 <jasonm23@gmail.com>
2013-03-25 02:14:45 +1100

    added cleaner

---
+++ javascripts/cleaner.js
@@ -0,0 +1,5 @@
+// Jenkins js tweaks
+setTimeout(function(){  
+  document.getElementById('search-box').setAttribute("type","search");
+  document.getElementById('main-table').setAttribute("style","none");
+}, 350);
