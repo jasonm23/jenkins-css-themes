af2cf637cbdd8336d49ecce99f93ccf9cdddfc6e
Jasonm23 <jasonm23@gmail.com>
2013-03-25 01:46:48 +1100

    try a longer timeout to remove jenkins image

--- javascripts/jenkins.js
+++ javascripts/jenkins.js
@@ -2,4 +2,4 @@
 setTimeout(function(){  
   document.getElementById('search-box').setAttribute("type","search");
   document.getElementById('main-table').setAttribute("style","none");
-}, 100);
+}, 350);
