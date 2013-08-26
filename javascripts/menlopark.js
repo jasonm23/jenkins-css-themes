fdcea49955dd8efa02f931c0167fc539171d3ae4
Jasonm23 <jasonm23@gmail.com>
2013-08-27 01:50:20 +1000

    added menlopark

---
+++ javascripts/menlopark.js
@@ -0,0 +1,5 @@
+// Jenkins js tweaks
+setTimeout(function(){
+  document.querySelector('#search-box').setAttribute("type","search");
+  document.querySelector('#main-table').setAttribute("style","none");
+}, 350);
