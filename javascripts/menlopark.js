26329f276e276e61c8ee7a9ee03379bdc95365fa
Jasonm23 <jasonm23@gmail.com>
2013-09-07 22:34:46 +1000

    Removed jQuery, "noconflict" is extremely confrontational with prototype...!
    Setup prototype equivalent to document.ready.

--- javascripts/menlopark.js
+++ javascripts/menlopark.js
@@ -1,28 +1,19 @@
-// Require JS
-var jq = document.createElement('script');
-jq.src = "http://code.jquery.com/jquery-latest.min.js";
-document.getElementsByTagName('head')[0].appendChild(jq);
-jQuery.noConflict();
+document.observe('dom:loaded', function() {
+  setTimeout(function(){
+    document.getElementById('search-box').setAttribute("type","search");
+    document.getElementById('main-table').setAttribute("style","none");
+    CodeMirror.defaults = {
+      theme: 'menlo',
+      smartIndent: true,
+      indentWithTabs: false,
+      keymap: 'emacs',
+      lineNumbers: true,
+      undoDepth: 100
+    }
 
-// Emacs codemirror keymap
-var emacs = document.createElement('script');
-emacs.src = "http://codemirror.net/keymap/emacs.js";
-document.getElementsByTagName('head')[0].appendChild(emacs);
-
-setTimeout(function(){
-  document.getElementById('search-box').setAttribute("type","search");
-  document.getElementById('main-table').setAttribute("style","none");
-}, 350);
-
-$(document).ready(function(){
-
-  CodeMirror.defaults = {
-    theme: 'menlo',
-    smartIndent: true,
-    indentWithTabs: false,
-    keymap: 'emacs',
-    lineNumbers: true,
-    undoDepth: 100
-  }
-
-})
+  }, 500);
+  // Emacs codemirror keymap
+  var emacs = document.createElement('script');
+  emacs.src = "http://codemirror.net/keymap/emacs.js";
+  document.getElementsByTagName('head')[0].appendChild(emacs);
+});
