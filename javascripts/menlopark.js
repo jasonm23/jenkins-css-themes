4ef1681bf2d3b44c8c8c0eeee2d7a28e128ac688
Jasonm23 <jasonm23@gmail.com>
2013-09-07 22:27:07 +1000

    Amendments..
    Added JQuery (no conflict mode)
    Added Emacs keymap (availability) for codemirror
    (switch it on in the browser Console for now)
    
    Placed timed triggers into document ready (experimental)
    
    Added CodeMirror defaults (undo depth 100, lineNumbers, indent without
    tabs, smart indent, menlo codemirror theme)

--- javascripts/menlopark.js
+++ javascripts/menlopark.js
@@ -1,5 +1,26 @@
-// Jenkins js tweaks
-setTimeout(function(){
+// Require JS
+var jq = document.createElement('script');
+jq.src = "http://code.jquery.com/jquery-latest.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
+jQuery.noConflict();
+
+// Emacs codemirror keymap
+var emacs = document.createElement('script');
+emacs.src = "http://codemirror.net/keymap/emacs.js";
+document.getElementsByTagName('head')[0].appendChild(emacs);
+
+$(document).ready(function(){
+
   document.querySelector('#search-box').setAttribute("type","search");
+
   document.querySelector('#main-table').setAttribute("style","none");
-}, 350);
+
+  CodeMirror.defaults = {
+    theme: 'menlo',
+    smartIndent: true,
+    indentWithTabs: false,
+    lineNumbers: true,
+    undoDepth: 100
+  }
+
+})
