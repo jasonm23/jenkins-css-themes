d9ec2d2c213549a5961a2404a8c41d1390f284c8
JasonM23 <jasonm23@users.noreply.github.com>
2015-06-26 10:45:42 +0800

    Hacky fix for build pipeline / fancybox

---
+++ javascripts/menlopark-fancybox.fix.js
@@ -0,0 +1,7 @@
+document.observe('dom:loaded', function() {
+  setTimeout(function(){
+    document.getElementById('search-box').setAttribute("type","search");
+  }, 500);
+});
+
+jQuery.browser = {};
