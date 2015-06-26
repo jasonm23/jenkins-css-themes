46214342a3581e6a9dd41054397f45fd84f133b6
JasonM23 <jasonm23@users.noreply.github.com>
2015-06-26 10:28:54 +0800

    main-table no longer in Jenkins +1.6

--- javascripts/menlopark.js
+++ javascripts/menlopark.js
@@ -1,6 +1,5 @@
 document.observe('dom:loaded', function() {
   setTimeout(function(){
     document.getElementById('search-box').setAttribute("type","search");
-    document.getElementById('main-table').setAttribute("style","none");
   }, 500);
 });
